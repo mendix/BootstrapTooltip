0e276a43a947c7dd5cd89ca8df49841227673805
Jelte Lagendijk <jelte.lagendijk@mendix.com>
2016-04-22 15:47:41 +0200

    Update development files & cleanup code

--- src/BootstrapTooltip/widget/BootstrapTooltip.js
+++ src/BootstrapTooltip/widget/BootstrapTooltip.js
@@ -1,104 +1,97 @@
-/*jslint white:true, nomen: true, plusplus: true */
-/*global mx, define, require, browser, devel, console, document */
-/*mendix */
-
 define([
-	"dojo/_base/declare",
-	"mxui/widget/_WidgetBase",
-	"dijit/_TemplatedMixin",
-	"mxui/dom",
-	"dojo/_base/lang",
-	"BootstrapTooltip/lib/jquery",
-	"BootstrapTooltip/lib/tooltip",
-	"dojo/text!BootstrapTooltip/widget/template/BootstrapTooltip.html"
-], function (declare, _WidgetBase, _TemplatedMixin, dom, dojolang, _jQuery, tooltip, widgetTemplate) {
-	"use strict";
+    "dojo/_base/declare",
+    "mxui/widget/_WidgetBase",
+    "dijit/_TemplatedMixin",
+    "mxui/dom",
+    "dojo/_base/lang",
+    "BootstrapTooltip/lib/jquery",
+    "BootstrapTooltip/lib/tooltip",
+    "dojo/text!BootstrapTooltip/widget/template/BootstrapTooltip.html"
+], function (declare, _WidgetBase, _TemplatedMixin, dom, lang, _jQuery, tooltip, widgetTemplate) {
+    "use strict";
 
-	var _jQ = _jQuery.noConflict(true);
-	var j$ = tooltip.createInstance(_jQ);
+    var _jQ = _jQuery.noConflict(true);
+    var j$ = tooltip.createInstance(_jQ);
 
-	// Declare widget"s prototype.
-	return declare("BootstrapTooltip.widget.BootstrapTooltip", [_WidgetBase, _TemplatedMixin], {
-		// _TemplatedMixin will create our dom node using this HTML template.
-		templateString: widgetTemplate,
+    return declare("BootstrapTooltip.widget.BootstrapTooltip", [_WidgetBase, _TemplatedMixin], {
 
-		// Parameters configured in the Modeler.
-		tooltipClassName: "",
-		tooltipMessageMicroflow: "",
-		tooltipMessageString: "",
-		tooltipLocation: "top",
-		tooltipMode: "hover",
+        templateString: widgetTemplate,
 
-		// Internal variables. Non-primitives created in the prototype are shared between all widget instances.
-		_tooltipText: "No custom text specified for this tooltip",
-		_tooltipTrigger: null,
+        tooltipClassName: "",
+        tooltipMessageMicroflow: "",
+        tooltipMessageString: "",
+        tooltipLocation: "top",
+        tooltipMode: "hover",
 
-		// dijit._WidgetBase.postCreate is called after constructing the widget. Implement to do extra setup work.
-		postCreate: function () {
+        _tooltipText: "No custom text specified for this tooltip",
+        _tooltipTrigger: null,
 
-			if (this.tooltipMode === "hover") {
-				this._tooltipTrigger = "focus hover";
-			} else if (this.tooltipMode === "focus") {
-				this._tooltipTrigger = "focus";
-			} else if (this.tooltipMode === "click") {
-				this._tooltipTrigger = "click";
-			}
-		},
+        postCreate: function () {
+            logger.debug(this.id + ".postCreate");
 
-		// mxui.widget._WidgetBase.update is called when context is changed or initialized. Implement to re-render and / or fetch data.
-		update: function (obj, callback) {
+            if (this.tooltipMode === "hover") {
+                this._tooltipTrigger = "focus hover";
+            } else if (this.tooltipMode === "focus") {
+                this._tooltipTrigger = "focus";
+            } else if (this.tooltipMode === "click") {
+                this._tooltipTrigger = "click";
+            }
+        },
 
-			if (this.tooltipMessageMicroflow !== "") {
-				mx.data.action({
-					params: {
-						actionname: this.tooltipMessageMicroflow
-					},
-					callback: dojolang.hitch(this, function (string) {
-						this._tooltipText = string;
-						this._initializeTooltip();
-					}),
-					error: dojolang.hitch(this, function (error) {
-						console.warn("Error executing Microflow: " + error);
-					})
-				}, this);
-			} else if (this.tooltipMessageString !== "") {
-				this._tooltipText = this.tooltipMessageString;
-				this._initializeTooltip();
-			} else {
-				this._initializeTooltip();
-			}
+        update: function (obj, callback) {
+            logger.debug(this.id + ".update");
 
-			callback();
-		},
+            if (this.tooltipMessageMicroflow !== "") {
+                mx.data.action({
+                    params: {
+                        actionname: this.tooltipMessageMicroflow
+                    },
+                    callback: lang.hitch(this, function (string) {
+                        this._tooltipText = string;
+                        this._initializeTooltip();
+                    }),
+                    error: lang.hitch(this, function (error) {
+                        console.warn("Error executing Microflow: " + error);
+                    })
+                }, this);
+            } else if (this.tooltipMessageString !== "") {
+                this._tooltipText = this.tooltipMessageString;
+                this._initializeTooltip();
+            } else {
+                this._initializeTooltip();
+            }
 
-		_initializeTooltip: function () {
+            callback();
+        },
 
-			// Find element by classname in the same container (DOM level) as widget
-			var $targetElement = j$(this.domNode).siblings("." +this.tooltipClassName);
+        _initializeTooltip: function () {
+            logger.debug(this.id + "._initializeTooltip");
 
-			// No element found on same level, try to find target element on page
-			if($targetElement.length===0) {
-				$targetElement = j$("." + this.tooltipClassName);
-			}
+            // Find element by classname in the same container (DOM level) as widget
+            var $targetElement = j$(this.domNode).siblings("." + this.tooltipClassName);
 
-			if($targetElement.length===0) {
-				console.warn("Did you configure BootstrapTooltip widget correctly? Couldn't find an element with class '"+ this.tooltipClassName +"' on same level as widget (id='" + this.domNode.id+"')" )
-			}
+            // No element found on same level, try to find target element on page
+            if ($targetElement.length === 0) {
+                $targetElement = j$("." + this.tooltipClassName);
+            }
 
-			//if the element is a label+input combination, find the input element.
-			if ($targetElement.hasClass("form-group")){
-				$targetElement = $targetElement.find(".form-control").length !== 0 ? $targetElement.find(".form-control") : $targetElement.find("input");
-			}
+            if ($targetElement.length === 0) {
+                console.warn("Did you configure BootstrapTooltip widget correctly? Couldn't find an element with class '"+ this.tooltipClassName +"' on same level as widget (id='" + this.domNode.id+"')" );
+            }
 
-			$targetElement.tooltip({
-				title: this._tooltipText,
-				placement: this.tooltipLocation,
-				trigger: this._tooltipTrigger,
-				html : this.tooltipRenderHTML
-			});
-		}
-	});
+            //if the element is a label+input combination, find the input element.
+            if ($targetElement.hasClass("form-group")){
+                $targetElement = $targetElement.find(".form-control").length !== 0 ? $targetElement.find(".form-control") : $targetElement.find("input");
+            }
+
+            $targetElement.tooltip({
+                title: this._tooltipText,
+                placement: this.tooltipLocation,
+                trigger: this._tooltipTrigger,
+                html : this.tooltipRenderHTML
+            });
+        }
+    });
 });
-require(["BootstrapTooltip/widget/BootstrapTooltip"], function () {
-	"use strict";
-});
+
+require(["BootstrapTooltip/widget/BootstrapTooltip"]);
--- src/BootstrapTooltip/widget/BootstrapTooltipContext.js
+++ src/BootstrapTooltip/widget/BootstrapTooltipContext.js
@@ -1,46 +1,41 @@
-/*jslint white:true, nomen: true, plusplus: true */
-/*global mx, define, require, browser, devel, console, document */
-/*mendix */
+define([
+    "dojo/_base/declare",
+    "dojo/_base/lang",
+    "BootstrapTooltip/widget/BootstrapTooltip"
+], function (declare, lang, _bootstrapTooltipWidget) {
+    "use strict";
 
-// Required module list. Remove unnecessary modules, you can always get them back from the boilerplate.
-require([
-    'dojo/_base/declare', 'dojo/_base/lang',
-    'BootstrapTooltip/widget/BootstrapTooltip'
-], function (declare, dojolang, _bootstrapTooltipWidget) {
-	"use strict";
+    return declare("BootstrapTooltip.widget.BootstrapTooltipContext", [_bootstrapTooltipWidget], {
 
-	// Declare widget"s prototype.
-	return declare("BootstrapTooltip.widget.BootstrapTooltipContext", [_bootstrapTooltipWidget], {
+        update: function (obj, callback) {
+            logger.debug(this.id + ".update");
 
-		// mxui.widget._WidgetBase.update is called when context is changed or initialized. Implement to re-render and / or fetch data.
-		update: function (obj, callback) {
-			var guid = obj.getGuid();
-			if (this.tooltipMessageMicroflow !== "") {
-				mx.data.action({
-					params: {
-						applyto: 'selection',
-						actionname: this.tooltipMessageMicroflow,
-						guids: [guid]
-					},
-					callback: dojolang.hitch(this, function (string) {
-						this._tooltipText = string;
-						this._initializeTooltip();
-					}),
-					error: dojolang.hitch(this, function (error) {
-						console.warn("Error executing Microflow: " + error);
-					})
-				}, this);
-			} else if (this.tooltipMessageString !== "") {
-				this._tooltipText = this.tooltipMessageString;
-				this._initializeTooltip();
-			} else {
-				this._initializeTooltip();
-			}
+            var guid = obj.getGuid();
+            if (this.tooltipMessageMicroflow !== "") {
+                mx.data.action({
+                    params: {
+                        applyto: "selection",
+                        actionname: this.tooltipMessageMicroflow,
+                        guids: [guid]
+                    },
+                    callback: lang.hitch(this, function (string) {
+                        this._tooltipText = string;
+                        this._initializeTooltip();
+                    }),
+                    error: lang.hitch(this, function (error) {
+                        console.warn("Error executing Microflow: " + error);
+                    })
+                }, this);
+            } else if (this.tooltipMessageString !== "") {
+                this._tooltipText = this.tooltipMessageString;
+                this._initializeTooltip();
+            } else {
+                this._initializeTooltip();
+            }
 
-			callback();
-		}
-	});
+            callback();
+        }
+    });
 });
-require(["BootstrapTooltip/widget/BootstrapTooltipContext"], function () {
-	"use strict";
-});
+
+require(["BootstrapTooltip/widget/BootstrapTooltipContext"]);
