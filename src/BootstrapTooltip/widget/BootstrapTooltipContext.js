2b6782cc6becbf4f4953b6119e9634d77500a1d4
Dennis Kho <dkho@flowfabric.com>
2016-12-13 14:54:53 +0100

    Added handling of tooltip attribute
    Added extra block to handle tooltip attribute so that a (context) entity's attribute can be targetted and rendered by the widget

--- src/BootstrapTooltip/widget/BootstrapTooltipContext.js
+++ src/BootstrapTooltip/widget/BootstrapTooltipContext.js
@@ -6,12 +6,18 @@ define([
     "use strict";
 
     return declare("BootstrapTooltip.widget.BootstrapTooltipContext", [_bootstrapTooltipWidget], {
+    _contextObj: null,
 
         update: function (obj, callback) {
             logger.debug(this.id + ".update");
 
             var guid = obj ? obj.getGuid() : null;
-            if (this.tooltipMessageMicroflow !== "") {
+            this._contextObj = obj;
+            var targetAttributeValue = this._contextObj.get(this.tooltipAttribute);
+            if (targetAttributeValue !== "") {
+                this._tooltipText = targetAttributeValue;
+                this._initializeTooltip();
+            } else if (this.tooltipMessageMicroflow !== "") {
                 this._execMf(this.tooltipMessageMicroflow, guid, lang.hitch(this, function (string) {
                     this._tooltipText = string;
                     this._initializeTooltip();
